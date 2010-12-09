1df7ae1138a4070c394278da1fee451363a5d1ad
Maciej Fijalkowski <fijall@gmail.com>
2010-12-09 15:05:32 +0200

    (simon, fijal) A small attempt at cleaning this script
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katsdisp/trunk@8197 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- scripts/pc_h5_capture.py
+++ scripts/pc_h5_capture.py
@@ -9,32 +9,43 @@
 # Regeneration of a SPEAD stream suitable for us in the online signal displays. At the moment this is basically
 # just an aggregate of the incoming streams from the multiple x engines scaled with n_accumulations (if set)
 
-import numpy as np, spead, logging, sys, time, h5py
+import numpy as np
+import spead
+import h5py
+import sys
+import time
+import optparse
 
-logging.basicConfig(level=logging.WARNING)
 
-acc_scale = False
- # scale by the reported number of accumulations per dump
-
-data_port = 7148
-sd_ip = '192.168.4.20'
+def parse_opts(argv):
+    parser = optparse.OptionParser()
+    parser.add_option('--ip', default='192.168.4.20',
+                      help='signal display ip')
+    parser.add_option('--data-port', default=7148, type=int,
+                      help='port to receive data on')
+    parser.add_option('--acc-scale', action='store_true', default=False,
+                      help='scale by the reported number of accumulations '
+                      'per dump')
+    return parser.parse(argv)
 
 def receive():
+    opts, args = parse_opts(sys.argv)
+    data_port = opts.data_port
+    acc_scale = opts.acc_scale
+    sd_ip = opts.ip
     print 'Initalising SPEAD transports...'
-    print "Data reception on port",data_port
+    print "Data reception on port", data_port
     rx = spead.TransportUDPrx(data_port, pkt_count=1024, buffer_size=51200000)
-    print "Sending Signal Display data to",sd_ip
+    print "Sending Signal Display data to", sd_ip
     tx_sd = spead.Transmitter(spead.TransportUDPtx(sd_ip, 7149))
     ig = spead.ItemGroup()
     ig_sd = spead.ItemGroup()
     f = h5py.File(str(int(time.time())) + ".pc.h5", mode="w")
-    data_ds = None
-    ts_ds = None
     idx = 0
     dump_size = 0
     datasets = {}
     datasets_index = {}
-    meta_required = ['n_chans','n_bls','n_stokes']
+    meta_required = set(['n_chans','n_bls','n_stokes'])
      # we need these bits of meta data before being able to assemble and transmit signal display data
     meta_desired = ['n_accs']
      # if we find these, then what hey :)
@@ -42,7 +53,6 @@ def receive():
     sd_frame = None
     sd_slots = None
     sd_timestamp = None
-    write_enabled = True
     for heap in spead.iterheaps(rx):
         ig.update(heap)
         for name in ig.keys():
@@ -53,24 +63,30 @@ def receive():
                 meta[name] = ig[name]
             if name in meta_required:
                 meta[name] = ig[name]
-                meta_required.pop(meta_required.index(name))
-                if len(meta_required) == 0:
+                meta_required.remove(name)
+                if not meta_required:
                     sd_frame = np.zeros((meta['n_chans'],meta['n_bls'],meta['n_stokes'],2),dtype=np.int32)
                     print "Initialised sd frame to shape",sd_frame.shape
-                    meta_required = ['n_chans','n_bls','n_stokes']
+                    meta_required = set(['n_chans','n_bls','n_stokes'])
                     sd_slots = None
-            if not datasets.has_key(name):
+            if not name in datasets:
              # check to see if we have encountered this type before
                 shape = ig[name].shape if item.shape == -1 else item.shape
                 dtype = np.dtype(type(ig[name])) if shape == [] else item.dtype
-                if dtype is None: dtype = ig[name].dtype
+                if dtype is None:
+                    dtype = ig[name].dtype
                  # if we can't get a dtype from the descriptor try and get one from the value
                 print "Creating dataset for name:",name,", shape:",shape,", dtype:",dtype
-                f.create_dataset(name,[1] + ([] if list(shape) == [1] else list(shape)), maxshape=[None] + ([] if list(shape) == [1] else list(shape)), dtype=dtype)
+                new_shape = list(shape)
+                if new_shape == [1]:
+                    new_shape = []
+                f.create_dataset(name,[1] + new_shape, maxshape=[None] +
+                                 new_shape, dtype=dtype)
                 dump_size += np.multiply.reduce(shape) * dtype.itemsize
                 datasets[name] = f[name]
                 datasets_index[name] = 0
-                if not item._changed: continue
+                if not item._changed:
+                    continue
                  # if we built from and empty descriptor
             else:
                 print "Adding",name,"to dataset. New size is",datasets_index[name]+1
@@ -106,7 +122,9 @@ def receive():
         if idx == 1:
             print "Repacking dataset",name,"as an attribute as it is singular."
             f['/'].attrs[name] = f[name].value[0]
-            f.__delitem__(name)
+            del f[name]
     f.flush()
     f.close()
-receive()
+
+if __name__ == '__main__':
+    receive()
