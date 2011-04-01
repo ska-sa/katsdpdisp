717846af1ef68ff37fe005a65eb68ed475835afb
Simon Cross <simon@ska.ac.za>
2011-04-01 10:14:56 +0200

    Add scripts to setup.py so they get installed.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katsdisp/trunk@9152 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup (
     description = "Karoo Array Telescope Online Signal Displays",
     author = "Simon Ratcliffe",
     packages = find_packages(),
-    scripts = [],
+    scripts = [
+        "scripts/k7_augment.py",
+        "scripts/k7_capture_katcp.py",
+        "scripts/k7_capture.py",
+        "scripts/k7_simulator.py",
+        ],
     zip_safe = False,
 )
