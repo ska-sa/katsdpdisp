b20e857264e6b12dadac1bf2d44e9731d19deeaf
Simon Ratcliffe <sratcliffe@ska.ac.za>
2011-05-25 14:40:40 +0200

    Added html support files for time_plot and made time plot installable. Part of preperation for splitting capture stuff out of katsdisp
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katsdisp/trunk@9685 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ setup (
     description = "Karoo Array Telescope Online Signal Displays",
     author = "Simon Ratcliffe",
     packages = find_packages(),
+    include_package_data = True,
     scripts = [
         "scripts/k7_augment.py",
         "scripts/k7_capture_katcp.py",
         "scripts/k7_capture.py",
         "scripts/k7_simulator.py",
+        "scripts/time_plot.py",
         ],
     zip_safe = False,
 )
