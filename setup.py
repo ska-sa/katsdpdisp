1df119a2f54c8c768b88f9ed6b118a0af0bff80f
Simon Ratcliffe <sratcliffe@ska.ac.za>
2011-05-25 14:51:27 +0200

    Move html under package directory.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katsdisp/trunk@9686 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup (
     description = "Karoo Array Telescope Online Signal Displays",
     author = "Simon Ratcliffe",
     packages = find_packages(),
+    package_dir = {'':'.'},
     include_package_data = True,
     scripts = [
         "scripts/k7_augment.py",
