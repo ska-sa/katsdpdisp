abc17a4284bf2eef89085df5f9e7873187e5e0d5
Mattieu de Villiers <mattieu@ska.ac.za>
2014-05-09 11:44:42 +0200

    Bug fix paste error
    <!--capture_server--> not pasted correctly

--- katsdpdisp/html/figure.js
+++ katsdpdisp/html/figure.js
@@ -1556,7 +1556,7 @@ function setsignals(){
       //ssh kat@obs.kat7.karoo 'cat id_rsa.pub >> .ssh/authorized_keys; rm id_rsa.pub'
       //handle_data_user_event('server,'+'ssh kat@obs.kat7.karoo \"python -c \'import katuilib; k7w=katuilib.build_client(\\\"k7w\\\",\\\"192.168.193.5\\\",2040,controlled=True); k7w.req.add_sdisp_ip(\\\"192.168.193.7\\\"); k7w.req.add_sdisp_ip(\\\"192.168.6.110\\\"); k7w.req.sd_metadata_issue();\'\"');
 	  
-      handle_data_user_event('server,'+'ssh kat@<!-- \"python -c \'import katuilib; k7w=katuilib.build_client(\\\"k7w\\\",\\\"192.168.193.5\\\",2040,controlled=True); k7w.req.add_sdisp_ip(\\\"192.168.6.54\\\"); k7w.req.add_sdisp_ip(\\\"192.168.193.7\\\"); k7w.req.add_sdisp_ip(\\\"192.168.6.110\\\"); k7w.req.sd_metadata_issue();\'\"');
+      handle_data_user_event('server,'+'ssh kat@<!--capture_server--> \"python -c \'import katuilib; k7w=katuilib.build_client(\\\"k7w\\\",\\\"192.168.193.5\\\",2040,controlled=True); k7w.req.add_sdisp_ip(\\\"192.168.6.54\\\"); k7w.req.add_sdisp_ip(\\\"192.168.193.7\\\"); k7w.req.add_sdisp_ip(\\\"192.168.6.110\\\"); k7w.req.sd_metadata_issue();\'\"');
       //'ssh kat@obs.kat7.karoo \"python -c \'import socket;rv=socket.gethostbyaddr(\\\"kat-dp2\\\");print rv[2];\'\"'
     }
     else
