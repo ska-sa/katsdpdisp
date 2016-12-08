e8c02fb7581bd218ef949013a9cd3fe5f1812373
Mattieu de Villiers <mattieu@ska.ac.za>
2016-12-08 11:10:57 +0200

    Add example in pythonclient.py script
    Add example in pythonclient.py script to determine port to array number.

--- scripts/pythonclient.py
+++ scripts/pythonclient.py
@@ -1,5 +1,5 @@
 from websocket import create_connection
-ws = create_connection("ws://10.98.2.101:8001")
+ws = create_connection("ws://10.98.2.111:8001")
 #set username
 ws.send(u"<data_user_event_timeseries args='setusername,pythonclient'>")
 result =  ws.recv()#ignore this reply
@@ -20,4 +20,31 @@ ws.send(u"<data_user_event_timeseries args='sendfiguredata,-1,200,201,mag,2h2h'>
 chan200 =  eval(ws.recv())
 print chan200
 
+#determine sub array number
+ws.send(u"<data_user_event_timeseries args='telstate,subarray_product_id,'>")
+subarrayproduct=ws.recv().split("\"")[1].split("'")[1]
+print 'product:',subarrayproduct
+
 ws.close()
+
+#scan ports to determine which port is used for which subarray
+from websocket import create_connection
+baseport=8000
+active_timeplots={}
+for portoffset in range(0,20,2):
+    try:
+        ws = create_connection("ws://10.98.2.111:%d"%(baseport+portoffset+1))
+    except Exception, e:
+        print 'port %d'%(baseport+portoffset+1),e
+        continue
+    #set username
+    ws.send(u"<data_user_event_timeseries args='setusername,portscannerclient'>")
+    result =  ws.recv()#ignore this reply
+    result2 =  ws.recv()#ignore this reply
+    ws.send(u"<data_user_event_timeseries args='telstate,subarray_product_id,'>")
+    subarrayproduct=ws.recv().split("\"")[1].split("'")[1]
+    print 'port:%d '%(baseport+portoffset)+'product:'+subarrayproduct
+    active_timeplots[baseport+portoffset]=subarrayproduct
+    ws.close()
+
+print active_timeplots
