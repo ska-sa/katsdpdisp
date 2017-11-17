fix issue in snr calculation loading data from h5
fix issue in snr calculation loading data from h5

katsdpdisp/data.py
@@ -592,7 +592,7 @@ class SignalDisplayStore2(object):
                                 if (thestd>0):
                                     self.timeseriessnrdata[self.timeseriesroll_point,iprod]=np.mean(mag[valid])*np.sqrt(2)/(thestd) # because of diff from previous sample, variance is doubled
                             else:
-                                self.timeseriessnrdata[self.timeseriesroll_point,:] = np.mean(mag[valid])/np.sqrt(timeseriesvar)
+                                self.timeseriessnrdata[self.timeseriesroll_point,iprod] = np.mean(mag[valid])/np.sqrt(timeseriesvar[iprod])
                 except Exception as e:
                     logger.warning('Exception in blmx calculation (blmxdata shape: %s): '%(repr(blmxdata.shape))+str(e), exc_info=True)
 