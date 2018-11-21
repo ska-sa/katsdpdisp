47abc1cadb0f79e25f31a2bebed25f86773fadb9
Mattieu de Villiers <mattieu@ska.ac.za>
2018-11-21 12:38:28 +0200

    wait rather than hope

--- scripts/time_plot.py
+++ scripts/time_plot.py
@@ -1921,8 +1921,8 @@ def handle_websock_event(handlerkey,*args):
                 send_websock_cmd('logconsole("Server exception occurred evaluating RESTART",true,true,true)',handlerkey)
             else:
                 send_websock_cmd('logconsole("Exit ring buffer process",true,true,true)',handlerkey)
-                time.sleep(2)
                 global rb_process
+                rb_process.wait()
                 rb_process = Process(target=RingBufferProcess,args=(opts.spead, opts.spead_port, opts.spead_interface, opts.memusage, opts.max_custom_signals, opts.datafilename, opts.cbf_channels, ringbufferrequestqueue, ringbufferresultqueue, ringbuffernotifyqueue))
                 rb_process.start()
                 logger.info('RESTART performed, using port=%d memusage=%f datafilename=%s'%(opts.spead_port,opts.memusage,opts.datafilename))
