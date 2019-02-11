534ecedd13a889266a95208b227932befe254528
Mattieu de Villiers <mattieu@ska.ac.za>
2019-02-11 11:14:28 +0200

    join instead of wait

--- scripts/time_plot.py
+++ scripts/time_plot.py
@@ -1922,7 +1922,7 @@ def handle_websock_event(handlerkey,*args):
             else:
                 send_websock_cmd('logconsole("Exit ring buffer process",true,true,true)',handlerkey)
                 global rb_process
-                rb_process.wait()
+                rb_process.join()
                 rb_process = Process(target=RingBufferProcess,args=(opts.spead, opts.spead_port, opts.spead_interface, opts.memusage, opts.max_custom_signals, opts.datafilename, opts.cbf_channels, ringbufferrequestqueue, ringbufferresultqueue, ringbuffernotifyqueue))
                 rb_process.start()
                 logger.info('RESTART performed, using port=%d memusage=%f datafilename=%s'%(opts.spead_port,opts.memusage,opts.datafilename))
@@ -2127,7 +2127,7 @@ def decodecustomsignal(signalstr):
 def printablesignal(product):
     a0=''.join(re.findall('[0-9]',product[0]))
     a1=''.join(re.findall('[0-9]',product[1]))
-    if len(a0) and len(a1):
+    if a0 and a1:
         return str(int(a0))+product[0][-1]+str(int(a1))+product[1][-1]
     else:#some error occurred; faulty signal
         return product
